8121c5012163f860d83df20229bc7e24454f0dcc
vishnu14125 <vishnu.n2023cse@sece.ac.in>
2025-08-12 23:56:13 +0530

    add DOCX parsing with mammoth

--- server/services/documentService.js
+++ server/services/documentService.js
@@ -1,10 +1,17 @@
 const pdfParse = require('pdf-parse');
+const mammoth = require('mammoth');
 
 async function parsePDF(buffer) {
   const data = await pdfParse(buffer);
   return data.text;
 }
 
+async function parseDOCX(buffer) {
+  const result = await mammoth.extractRawText({ buffer });
+  return result.value;
+}
+
 module.exports = {
-  parsePDF
+  parsePDF,
+  parseDOCX
 };
