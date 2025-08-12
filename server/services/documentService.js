0dee437aba061df21459b6507da54381a165f4ab
vishnu14125 <vishnu.n2023cse@sece.ac.in>
2025-08-12 23:57:16 +0530

    add TXT parsing and generic parseDocument function

--- server/services/documentService.js
+++ server/services/documentService.js
@@ -1,6 +1,19 @@
 const pdfParse = require('pdf-parse');
 const mammoth = require('mammoth');
 
+async function parseDocument(buffer, mimeType) {
+  switch (mimeType) {
+    case 'application/pdf':
+      return await parsePDF(buffer);
+    case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+      return await parseDOCX(buffer);
+    case 'text/plain':
+      return parseTXT(buffer);
+    default:
+      throw new Error(`Unsupported file type: ${mimeType}`);
+  }
+}
+
 async function parsePDF(buffer) {
   const data = await pdfParse(buffer);
   return data.text;
@@ -11,7 +24,13 @@ async function parseDOCX(buffer) {
   return result.value;
 }
 
+function parseTXT(buffer) {
+  return buffer.toString('utf-8');
+}
+
 module.exports = {
+  parseDocument,
   parsePDF,
-  parseDOCX
+  parseDOCX,
+  parseTXT
 };
